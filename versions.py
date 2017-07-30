4ba2fef62e20901ceeceaed1b39bfbdbb67f2098
Umesh <ushisode1@gmail.com>
2017-07-30 15:58:09 +0530

    minor updates

--- versions.py
+++ versions.py
@@ -1,3 +1,23 @@
 # scipy
 import scipy
 print("scipy: %"%scipy.__version__)
+
+# numpy
+import numpy
+print("numpy: %"%numpy.__version__)
+
+# pandas
+import pandas
+print("pandas: %"%pandas.__version__)
+
+# matplotlib
+import matplotlib
+print("matplotlib: %"%matplotlib.__version__)
+
+# statsmodels
+import statsmodels
+print("statsmodels: %"%statsmodels.__version__)
+
+# sklearn
+import sklearn
+print("sklearn: %"%sklearn.__version__)
